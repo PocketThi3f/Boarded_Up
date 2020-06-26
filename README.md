c8ade2864e82b3beb1e800ef9d48818905e4bcf7
Kris <kris@Kriss-MacBook-Pro.local>
2020-06-25 18:03:07 -0700

    edit readme and create js file for image cards

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ A website dedicated to my board gaming addiction
 * HTML
 * CSS (Flexbox methodology)
 * JS
+* Potential Changes to either Tailwind.css or Bulma
 
 
 ## Host
---
+++ js/bgCard.js
@@ -0,0 +1,1 @@
+// This file is intended for creating an image card, that populates the middle content field, to add more and more board game images in the middle of the index page.
