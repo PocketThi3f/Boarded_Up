4a67bc5200970ad4f6156fab43d4274b53bb242b
Kris <kris@Kriss-MacBook-Pro.local>
2020-06-28 18:38:55 -0700

    trying to manipulate DOM with JS

--- js/bgCard.js
+++ js/bgCard.js
@@ -1,16 +1,14 @@
 // Click to add a mini review card to the DOM
 function createCard() {
     // Local variable
-    var card, container;
+    var elementDiv, elementPara, elementImg, elementLink;
 
-    container = document.getElementsByClassName("container");
-    for (card = 0; card < container.length; card++) {
-        <div class="bg-card">
-	        <p>Title of Board Game</p>
-	        <img src="./images/azul-bg.jpg" alt="A cool board game" />
-	        <a href=""><p>Link to Board Game Description and Review</p></a>
-        </div>
-    }
+    // Button creates div element with classname of bg-card
+    cardBtn = document.createElement("div").className = "bg-card";
+
+    // Appends the new element into a div with classname container
+    document.body.getElementsByClassName("container").appendChild(cardBtn);
+	        // <p>Title of Board Game</p>
+	        // <img src="./images/azul-bg.jpg" alt="A cool board game" />
+	        // <a href=""><p>Link to Board Game Description and Review</p></a>
 }
-
-// Select that card container to open with reviews
