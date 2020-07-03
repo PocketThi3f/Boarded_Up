25c8ef40a75137c4dbb205b42e44f60ac7d6c895
Kris <kris@Kriss-MacBook-Pro.local>
2020-07-03 16:28:11 -0700

    missing a final step for input value

--- js/bgCard.js
+++ js/bgCard.js
@@ -1,15 +1,19 @@
 // Click to add a mini review card to the DOM
 function inputValue() {
 
+    // Looking for listened event
+    // console.log(window);
+
+    // Select id of input for and listen for user input
     var value = document.getElementById('gameInput').value;
 
-    // Input Test
-    // alert(value);
+    console.log(value);
 
-    var div = document.createElement('div').getElementsByClassName('content');
+    document.body.addEventListener('click', function() {
+        var div = document.createElement('div');
 
-    // document.body.append(div);
+        div.innerHTML = '<p></p>';
 
-	//         // <img src="./images/azul-bg.jpg" alt="A cool board game" />
-    //         // <a href=""><p>Link to Board Game Description and Review</p></a>
+        document.body.append(div);
+    });
 }
