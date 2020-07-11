27613daa6c18a25d7d1e57f17242759855af8168
Kris <kris@Kriss-MacBook-Pro.local>
2020-07-10 23:49:17 -0700

    added content to about page

--- js/index.js
+++ js/index.js
@@ -17,7 +17,7 @@ function openCity(cityName, elmnt, color) {
   
     // Add the specific color to the button used to open the tab content
     elmnt.style.backgroundColor = color;
-  }
+}
   
-  // Get the element with id="defaultOpen" and click on it
-  document.getElementById("defaultOpen").click();
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
