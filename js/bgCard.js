0c5ab05dda07bfdcdeda04fb7a5841c1c3e33b05
Kris Lai <laikxzhi@gmail.com>
2020-07-24 17:53:20 -0700

    testing and debugging js files

--- js/bgCard.js
+++ js/bgCard.js
@@ -1,3 +1,22 @@
 // Start off by creating a var to log user input value to console
-var value = document.getElementById("gameInput");
+var value = document.getElementById("gameInput").value;
 
+// Log the value to console
+console.log(value);
+
+// Create a var to capture button click
+var btnClick = document.getElementById("addItem");
+
+// Create function that creates a new element
+function addItem() {
+    var newDiv = document.createElement("div");
+
+    // Append value into new div
+    newDiv.append(value);
+
+    // Append new div with user input value into body of the page
+    document.body.append(newDiv);
+}
+
+// Main function that listens for user click event for adding game items
+btnClick.addEventListener('click', addItem);
