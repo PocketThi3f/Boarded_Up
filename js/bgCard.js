7843b2c0b81b433b275f18574f75f95292b85601
Kris Lai <laikxzhi@gmail.com>
2020-07-24 17:46:33 -0700

    started card js from scratch

--- js/bgCard.js
+++ js/bgCard.js
@@ -1,21 +1,3 @@
-// Click to add a mini review card to the DOM
-function inputValue() {
+// Start off by creating a var to log user input value to console
+var value = document.getElementById("gameInput");
 
-    // Looking for listened event
-    // console.log(window);
-
-    // Select id of input for and listen for user input
-    var value = document.getElementById('gameInput').value;
-
-    // console.log(value);
-
-    // Get id of button that event will listen for
-    var newItem = document.getElementById('listItem');
-
-    // Creating a function that allows a new list item to appear from userInput
-    function createGameCard() {
-        
-    }
-
-    document.createElement('div').addEventListener('click',)
-}
