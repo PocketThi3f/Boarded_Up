f08dd6873564194ac665c7ff7b4a5bf522fca047
Kris Lai <laikxzhi@gmail.com>
2020-07-31 15:54:04 -0700

    making changes to id and class for index page

--- js/addGame.js
+++ js/addGame.js
@@ -1,7 +1,15 @@
-// Select form by id
+// Select element by id
 const gameItem = document.getElementById("gameForm");
+const gameTable = document.getElementById("game-table");
+const inputBox = document.getElementsByClassName("inputField");
 
 // Log result to console
 console.log(gameItem);
 
+// Function for web page reloading
+function addGameToTable (event) {
+    event.preventDefault();
+}
 
+// Add an event listener function for whenever the user clicks the button
+gameItem.addEventListener('click', addGameToTable);
