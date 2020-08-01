3a83e0a4e8cb51eb0352aa7c1f832fdc3664f6b4
Kris Lai <laikxzhi@gmail.com>
2020-07-31 18:38:45 -0700

    created variables for adding new divs

--- js/addGame.js
+++ js/addGame.js
@@ -1,13 +1,24 @@
 // Select element by id
-const gameItem = document.getElementById("gameForm");
+const gameForm = document.getElementById("gameForm");
 const gameTable = document.getElementById("game-table");
 const inputBox = document.getElementsByClassName("inputField");
 
 // Log result to console
-console.log(gameItem);
+console.log(gameForm);
+console.log(inputBox.value);
 
 // Function for web page reloading
 function addGameToTable (event) {
+
+    // Variable that creates a new div container and give it a class name of game-item
+    const newDiv = document.createElement("div");
+
+    newDiv.className = "game-item";
+
+    // Manipulate element's innerText property
+    newDiv.innerText = inputBox[0].value;
+
+    // Keep page from reloading on click
     event.preventDefault();
 }
 
