1489f0582a3be1a1fa5d0054f36d7247535e1a3f
Kris Lai <laikxzhi@gmail.com>
2020-08-06 16:01:53 -0700

    finding bugs in index js file

--- js/addGame.js
+++ js/addGame.js
@@ -1,14 +1,17 @@
 // Select element by id
 const gameForm = document.getElementById("gameForm");
-const gameTable = document.getElementById("game-table");
-const inputBox = document.getElementsByClassName("inputField");
+const gameTable = document.querySelector("#game-table");
+const inputBox = document.querySelector(".inputField");
+const gameNav = document.querySelector(".game-nav");
 
 // Log result to console
 console.log(gameForm);
-console.log(inputBox.value);
+// console.log(inputBox[0].value);
 
 // Function for web page reloading
 function addGameToTable (event) {
+    // Keep page from reloading on click
+    event.preventDefault();
 
     // Variable that creates a new div container and give it a class name of game-item
     const newDiv = document.createElement("div");
@@ -16,20 +19,27 @@ function addGameToTable (event) {
     // Add class name to newly created divs
     newDiv.className = "game-item";
 
-    // Manipulate element's innerText property
-    newDiv.innerText = inputBox[0].value;
+    // Manipulate element's innerText property and set to empty string after submit
+    newDiv.innerText = inputBox.value;
+    newDiv.style.display = 'none';
+    inputBox.value = '';
 
     // Add the item into given element with id of game-table
     gameTable.appendChild(newDiv);
 
-    // Keep page from reloading on click
-    event.preventDefault();
+    // Make a new variable to put the element as a button to display game information
+    const gameDisplayButton = document.createElement("button");
+    gameDisplayButton.className = "game-display-button";
+    gameDisplayButton.innerText = "I am a game";
+
+    // Append display game button into new game-nav div
+    gameNav.appendChild(gameDisplayButton);
 }
 
 // Create a for loop to put data into an empty array and allow divs to populate normally
-for(var i = 0; i < gameTable.length; i++) {
-    console.log(gameTable[i]);
-}
+// for(var i = 0; i < gameNav.length; i++) {
+//     console.log(gameNav[i]);
+// }
 
-// Add an event listener function for whenever the user clicks the button
-gameForm.addEventListener('click', addGameToTable);
+// Add an event listener function for whenever the user submits the button
+gameForm.addEventListener('submit', addGameToTable);
